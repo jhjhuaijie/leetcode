c5a868ea6dd9918f0a4ac0c8c6808f1ac71fc4cf
jhjhuaijie <https://github.com/jhjhuaijie/>
2022-05-31 17:08:39 +0800

    547. 省份数量 并查集解法

--- union_find/main.cpp
+++ union_find/main.cpp
@@ -60,16 +60,71 @@ int FindCircleNumByBfs(std::vector<std::vector<int>>& isConnected)
 }
 #pragma endregion
 
+#pragma region 并查集
+// 初始化并查集 节点的父节点是其本身
+void Init(std::vector<int>& parent)
+{
+    int num = parent.size();
+    for (int i = 0; i < num; i++) {
+        parent[i] = i;
+    }
+}
 
+// 查找集合的根节点，根节点的父节点是其本身，作为一个集合的标志
+int Find(std::vector<int>& parent, int index)
+{
+    if (parent.empty() || !(index < parent.size())) {
+        std::cout << "error" << std::endl;
+        return 0;
+    }
+    // 压缩路径
+    if (parent[index] != index) {
+        parent[index] = Find(parent, parent[index]);
+    }
+    return parent[index];
+}
+
+// 合并两个集合，找到根节点
+void Union(std::vector<int>& parent, int index1, int index2)
+{
+    parent[Find(parent, index1)] = Find(parent, index2);
+}
+
+int FindCircleNumByUnionFind(std::vector<std::vector<int>>& isConnected)
+{
+    int num = isConnected.size();
+    std::vector<int> parent(num);
+    Init(parent);
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
+            if (i == j) {
+                continue;
+            }
+            if (isConnected[i][j] == 1) {
+                Union(parent, i, j);
+            }
+        }
+    }
+
+    int provinces = 0;
+    for (int i = 0; i < num; i++) {
+        if (parent[i] == i) {
+            provinces++;
+        }
+    }
+
+    return provinces;
+}
+#pragma endregion
 #pragma endregion
 int main()
 {
     std::vector<std::vector<int>> isconnected {
-        {1, 1, 0},
-        {1, 1, 0},
+        {1, 0, 0},
+        {0, 1, 0},
         {0, 0, 1}
     };
     std::cout << "province: ";
-    std::cout << FindCircleNumByBfs(isconnected) << std::endl;
+    std::cout << FindCircleNumByUnionFind(isconnected) << std::endl;
     return 0;
 }
